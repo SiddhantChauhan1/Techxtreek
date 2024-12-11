8aac3b27fe5f6e40101d6994cae1ffaa3fadc8d2
SiddhantChauhan1 <siddhantchauhan003@gmail.com>
2024-12-12 05:09:50 +0530

    fixing errors

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs/server";
 
 import UserCard from "@/components/cards/UserCard";
-import Searchbar from "@/components/shared/Searchbar";
+import Searchbar from "@/components/shared/SearchBar";
 import Pagination from "@/components/shared/Pagination";
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
